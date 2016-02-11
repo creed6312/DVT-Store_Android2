39efe1739ed732b5d559baab7a7ee3c48850c61d
Shane Michael Sardinha <creed.shane@gmail.com>
2016-02-11 07:17:25 +0200

    added search button in toolbar

--- app/src/main/java/com/jhb/dvt/dvt_store/MainActivity.java
+++ app/src/main/java/com/jhb/dvt/dvt_store/MainActivity.java
@@ -166,17 +166,23 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
-
+        else if (id == R.id.action_settings) {
+            doSearch();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void doSearch()
+    {
+
     }
 
     public class SimpleItemRecyclerViewAdapter extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
